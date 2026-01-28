8b169ca05e2748c45df4cd3a4c79ca193950f31a
Pochochi01 <largomauroandres@hotmail.com>
2026-01-28 12:34:27 -0300

    mostrar por consola direcciones que de login

--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -9,8 +9,6 @@ const initialState = {
 
 
 
-
-
 export const registerUser = createAsyncThunk(
   "/auth/register",
   async (formData) => {
@@ -26,6 +24,8 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
     "/auth/login",
     async (formData) => {
+      console.log(import.meta.env.VITE_API_URL);
+      console.log(`${import.meta.env.VITE_API_URL}/api/auth/login`);
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/api/auth/login`,
         formData,
